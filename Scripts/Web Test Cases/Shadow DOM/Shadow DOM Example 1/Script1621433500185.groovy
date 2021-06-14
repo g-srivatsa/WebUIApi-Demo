666d754f167e0bae10f705bd21ddddd81f29f0ec
g-srivatsa <83597486+g-srivatsa@users.noreply.github.com>
2021-06-14 15:21:53 -0600

    Second set of files

---
+++ Scripts/Web Test Cases/Shadow DOM/Shadow DOM Example 1/Script1621433500185.groovy
@@ -0,0 +1,31 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.github.kklisura.cdt.protocol.events.dom.ShadowRootPopped as ShadowRootPopped
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.common.WebUiCommonHelper as WebUiCommonHelper
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import applicationFunction.CustomFunction as CustomFunction
+import internal.GlobalVariable as GlobalVariable
+
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('https://books-pwakit.appspot.com/')
+
+WebUI.maximizeWindow()
+
+WebUI.setText(findTestObject('Web Object Repository/Shadow DOM/LoginChildDOM'), 'Automation')
+
+WebUI.sendKeys(findTestObject('Web Object Repository/Shadow DOM/LoginChildDOM'), Keys.chord(Keys.ENTER))
+
